81bee3fbd251b82eac5234f3ecbcdde737c2ada9
Nipun Dilshan <nipundilshan991@gmail.com>
2024-12-03 23:00:00 +0530

    product

--- src/components/Product.js
+++ src/components/Product.js
@@ -20,7 +20,15 @@ export default function Product(props){
         <div className="grid-item">
 
             <div class="card">
-               
+            <img src={require(`../assets/image/${props.img}`)} />
+                <div class="card-body">
+                    <h5 class="card-title">{props.name}Price:{props.price}</h5>
+                    <div class="quantity-container">
+                        <label for="quantity">Quantity:</label>
+                        <input type="number" id="quantity" name="quantity" min={0} value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}/>
+                    </div>
+                    <button class="card-button" onClick={handleAddToCart}>Add to Cart</button>
+                </div>
                 
             </div>
         </div>
