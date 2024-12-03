138bcc3022822fc7dd4789e16eaa87b242fc9fb5
Nipun Dilshan <nipundilshan991@gmail.com>
2024-12-03 22:59:35 +0530

    product

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,7 +2,18 @@ import { useState } from 'react';
 import '../assets/CSS/layout.css';
 
 export default function Product(props){
-    
+    const [quantity, setQuantity] = useState(0)
+
+    const handleAddToCart = () => {
+        if (quantity > 0) {
+            props.addToCart({
+                name: props.name,
+                price: props.price * quantity,
+                qty: quantity,
+            })
+            setQuantity(0)
+        }
+    }
     
     
     return(
